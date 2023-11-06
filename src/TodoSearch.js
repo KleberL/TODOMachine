50d4ce2fb4ac5ee36e92a50f4f6f843667a72af4
KleberL <kleberlandivar93@gmail.com>
2023-11-05 19:12:12 -0500

    first commit

---
+++ src/TodoSearch.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './TodoSearch.css'
+
+function TodoSearch({
+  searchValue,
+  setSearchValue
+}){
+    return(
+      <input 
+        placeholder="Cortar cebolla"
+        className="TodoSearch"
+        value={searchValue}
+        onChange={(e) => {
+          setSearchValue(e.target.value)
+        }}
+      />
+    );
+  }
+
+export {TodoSearch};
